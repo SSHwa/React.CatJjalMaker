1823fd445d6c794945d10d8e010ee7c70f92b6c4
showjf <showjf2@gmail.com>
2022-08-24 16:10:21 +0900

    CRA 생성2

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -37,7 +37,7 @@ function Form({ updateMainCat }) {
         value={value}
         onChange={handleInputChange}
       />
-      <button type="submit"> 생성 </button>
+      <button type="submit"> 생성 2</button>
       <p style={{ color: "red" }}>{errorMessage}</p>
     </form>
   );
